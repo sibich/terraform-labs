7597ac2df747907dc9d0bbc2f3ba45cd0a1309a3
Vitalii Sibichenkov <sibich.v@gmail.com>
2019-07-07 13:58:50 +0300

    test

--- test_custom.ps1
+++ test_custom.ps1
@@ -1,1 +1,4 @@
-﻿new-item c:\Test -ItemType directory -Force
+﻿new-item D:\soft -ItemType directory -Force
+Invoke-WebRequest -Uri https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe -OutFile D:\soft\note.exe
+Install-Module -Name Az -Force
+Start-Process 'D:\soft\note.exe' "/S"
