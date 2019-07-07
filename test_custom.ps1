8139b77f4087453f3443141816c453bc6b7019e4
Vitalii Sibichenkov <sibich.v@gmail.com>
2019-07-07 15:39:38 +0300

    test

--- test_custom.ps1
+++ test_custom.ps1
@@ -1,4 +1,6 @@
 ﻿new-item D:\soft -ItemType directory -Force
 Invoke-WebRequest -Uri https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe -OutFile D:\soft\note.exe -UseBasicParsing
+Invoke-WebRequest -Uri http://az764295.vo.msecnd.net/stable/c7d83e57cd18f18026a8162d042843bda1bcf21f/VSCodeSetup-x64-1.35.1.exe -OutFile  D:\soft\vscode.exe -UseBasicParsing
 #Install-Module -Name Az -Force
 & D:\soft\note.exe /S
+& D:\soft\vscode.exe /VERYSILENT /NORESTART /MERGETASKS=!runcode
